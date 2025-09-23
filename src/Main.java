5027a3f29a3cccf33925821e7cc5208281a0aa8e
Havard-Tonning <havton24@gmail.com>
2025-09-23 21:08:13 +0300

    IBM model 1

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 //
 //
 //        InputReader reader = new InputReader(trie);
-//
 //        reader.readAndInsertWords("src/names.txt");
 //
 //        System.out.println(trie.search("Aasta"));
@@ -14,16 +13,18 @@ public class Main {
 
 //         InputToCSV csvReader = new InputToCSV();
 
-        ArrayList<String[]> couples = Train.ReadCSV("src/pairs.csv");
-        System.out.println("Loaded " + couples.size() + " pairs from CSV");
-
-        ArrayList<String[]> couples2 = Train.ReadTSV("src/npk_2011_2022.tsv");
-        System.out.println("Loaded " + couples2.size() + " pairs from TSV");
+//        ArrayList<String[]> couples = Train.ReadCSV("src/pairs.csv");
+//        System.out.println("Loaded " + couples.size() + " pairs from CSV");
+//
+//        ArrayList<String[]> couples2 = Train.ReadTSV("src/npk_2011_2022.tsv");
+//        System.out.println("Loaded " + couples2.size() + " pairs from TSV");
 
 //        System.out.println(couples[50][0]);
 //        System.out.println(couples[50][1]);
 
-        Train.generateTranslation(couples2);
+//        Train.generateTranslation(couples2);
+
+        new Translate("This is a test, but it does not have to be");
 
     }
 }

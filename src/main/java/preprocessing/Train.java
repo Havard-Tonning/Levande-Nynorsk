465c6f8b1b303936ff865fb397195627f43df14a
Havard-Tonning <havton24@gmail.com>
2025-11-26 21:06:43 +0200

    Minor bug fixes.

--- src/main/java/preprocessing/Train.java
+++ src/main/java/preprocessing/Train.java
@@ -205,9 +205,9 @@ public class Train {
     public static void AppendBannedWords(){
         try (FileWriter writer = new FileWriter("src/main/java/preprocessing/translation.csv", true)) {
             String[] aWords = {"jeg", "også", "hun", "ham", "ikke", "de", "dere", "fra", "da", "en", "et", "hvor", "noen", "man", "dem", "kommer", "ble",
-                    "sendes", "hvorav", "verdenskrig", "verdenskrigen","Norge", "enten", "forsetter", "åringer", "fremgår"};
+                    "sendes", "hvorav", "verdenskrig", "verdenskrigen","Norge", "enten", "forsetter", "åringer", "fremgår", "fortsetter", "fortsette"};
             String[] bWords = {"eg", "òg", "ho", "han", "ikkje", "dei", "dykk", "frå", "då", "ein", "eit", "kor", "nokon", "ein",
-                    "dei", "kjem", "vart", "sendast", "kor", "verdskrig", "verdskrigen","Noreg", "anten", "held fram", "åringar", "går fram"};
+                    "dei", "kjem", "vart", "sendast", "kor", "verdskrig", "verdskrigen","Noreg", "anten", "held fram", "åringar", "går fram", "held fram", "halde fram"};
 
             for(int i = 0; i < aWords.length; i++){
                 writer.append(aWords[i] + "," + bWords[i] + ",1\n");
